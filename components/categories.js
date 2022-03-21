e2baa815685dec33b764053a2140e0485f26d61c
Codablack23 <goodluckedih@yahoo.com>
2022-03-21 19:00:08 +0100

    3 files added

---
+++ components/categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Categories({categories}){
+  return(
+    <div>
+      <h1>{categories.name}</h1>
+      <div className="categories">
+        
+      </div>
+    </div>
+  )
+}
---
+++ components/movies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Movies(movie){
+ return(
+   <div>
+    <p className="movie-name">{movie}</p>
+   </div>
+ )
+}
