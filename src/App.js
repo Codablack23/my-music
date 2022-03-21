99d21102ad3e7ace0a4fe1a0acbb0b3a5135e630
Codablack23 <goodluckedih@yahoo.com>
2022-03-21 19:06:33 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <Categories />
+      <Categories categories={{ name: 'Movie' }} />
     </div>
   );
 }
